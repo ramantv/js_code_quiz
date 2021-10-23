fd309514029f19ee55bd8a0506a4a10c0150597d
ramantv <90482664+ramantv@users.noreply.github.com>
2021-10-22 20:13:56 -0500

    version 0.6 - Highscores storage and display completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,10 @@ var ansGoodEl = document.getElementById("ans-good");
 
 var endQuizContainerEl = document.getElementById("end-quiz-container");
 var finalScoreValueEl = document.getElementById("final-score-value");
+var formSubmitScore = document.getElementById("submit-score-form");
 
+var hiScoresContainerEl = document.getElementById("high-scores-container");
+var listHighScoreEl = document.getElementById("high-score-list");
 
 // Global variables
 var curQn = 0; // tracks the current question
@@ -23,6 +26,8 @@ var timeLeft = 120; // the time left in the quiz, starts at 120 seconds
 const penalty = 10; // seconds penalized for wrong answers
 var timerInterval = 0;
 var gameOver = false;
+var finalScore = 0;
+var highScoresArray = [];
 
 // check that question bank is loaded from questions.js
 console.log(questionBank);
@@ -40,7 +45,7 @@ function startQuiz() {
 
 // Displays the question and answer choices from the quizQuestions array at the given index
 var displayQuestion = function (index) {
-  questionTextEl.innerText = "" + (index+1) + ". " + quizQuestions[index].qn;
+  questionTextEl.innerText = "" + (index + 1) + ". " + quizQuestions[index].qn;
   var answerbutton;
   for (var i = 0; i < quizQuestions[index].choices.length; i++) {
     answerbutton = document.createElement("button");
@@ -58,15 +63,14 @@ var checkAnswer = function (event) {
   var selectedAnswer = event.target.innerText;
   var goodAnswer = quizQuestions[curQn].choices[quizQuestions[curQn].ans];
   console.log("selected answer = " + selectedAnswer);
-  console.log("correct answer = " + goodAnswer );
+  console.log("correct answer = " + goodAnswer);
 
-  // if the right answer was chosen show the correct answer message on the screen, 
+  // if the right answer was chosen show the correct answer message on the screen,
   // else show the wrong answer message. If the answer was wrong apply the time penalty as well.
   if (selectedAnswer === goodAnswer) {
     console.log("Correct answer chosen!!");
     showAnswerRightOrWrongMessage(curQn, true);
-  } 
-  else {
+  } else {
     console.log("Wrong answer chosen!!");
     showAnswerRightOrWrongMessage(curQn, false);
     applyPenalty();
@@ -77,27 +81,103 @@ var checkAnswer = function (event) {
   if (curQn < quizQuestions.length) {
     resetQuestionContainer();
     displayQuestion(curQn);
-  }
-  else {
+  } else {
     gameOver = true;
+    finalScore = timeLeft;
+
     // show the scores container
-    showEndQuizContainer();
+    showEndQuizContainer(finalScore);
   }
 };
 
 // to show the final score and allow the user to submit it to the high scores.
-function showEndQuizContainer() {
-
+function showEndQuizContainer(finalScore) {
   // show the final score (which is the time left)
-  finalScoreValueEl.innerHTML = " " + timeLeft;
+  finalScoreValueEl.innerHTML = " " + finalScore;
 
   // hide all the other containers.
   showElement(startQuizContainerEl, false);
   showElement(questionContainerEl, false);
   showElement(ansKeyContainerEl, false);
+  showElement(hiScoresContainerEl, false);
 
-  // show the end-quiz-container
+  // show the hi-scores-container
   showElement(endQuizContainerEl, true);
+}
+
+//create high score values
+var saveHighScore = function (event) {
+  event.preventDefault();
+  var initials = document.querySelector("#initials").value;
+  if (!initials) {
+    alert("Enter your intials!");
+    return;
+  }
+
+  // reset the form
+  formSubmitScore.reset();
+
+  // add the final score to the High scores array with the users initials
+  addToHighScores(initials, finalScore);
+
+  // save the high scores to local storage
+  saveHighScoresToLocalStorage(highScoresArray);
+
+  // create the list elements to show the high scores
+  setupHighScoresContainer(highScoresArray);
+
+  // show the high scores container
+  showHighScoresContainer();
+};
+
+function setupHighScoresContainer(hiScrArr) {
+  //clear visibile list to resort
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
+
+  // create elements in order of high scores 
+  // the hiScrArr should already be setup in desc order
+  for (var i = 0; i < hiScrArr.length; i++) {
+    var highscoreEl = document.createElement("li");
+    highscoreEl.ClassName = "high-score";
+    highscoreEl.innerHTML = hiScrArr[i].userName + " - " + hiScrArr[i].score;
+    listHighScoreEl.appendChild(highscoreEl);
+  }
+}
+
+function showHighScoresContainer() {
+  // hide all the other containers.
+  showElement(startQuizContainerEl, false);
+  showElement(questionContainerEl, false);
+  showElement(ansKeyContainerEl, false);
+  showElement(endQuizContainerEl, false);
+
+  // show only the high-scores-container
+  showElement(hiScoresContainerEl, true);
+}
+
+// helper function to create the Array of highscores and keep it sorted
+function addToHighScores(initials, finScore) {
+  var lastHighScoreAdded = {
+    userName: initials,
+    score: finScore,
+  };
+
+  //push high score into highScoresArray.
+  highScoresArray.push(lastHighScoreAdded);
+
+  // Sort highScoresArray in descending order of high scores
+  highScoresArray.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  return lastHighScoreAdded;
+}
+
+//save high scores to local storage
+function saveHighScoresToLocalStorage(hiScAr) {
+  localStorage.setItem("HighScores", JSON.stringify(hiScAr));
 }
 
 // before showing the next question, ensure that the buttons for the previous questions are removed.
@@ -111,20 +191,20 @@ function resetQuestionContainer() {
 // to show if the previous question's answer was "Correct" or "Wrong"
 function showAnswerRightOrWrongMessage(qnIdx, booleanRight) {
   if (booleanRight) {
-    ansGoodEl.innerHTML = "" + (qnIdx+1) + ". " + "Correct &#9989";
+    ansGoodEl.innerHTML = "" + (qnIdx + 1) + ". " + "Correct &#9989";
     showElement(ansKeyContainerEl, true);
   } else {
-    ansGoodEl.innerHTML = "" + (qnIdx+1) + ". " + "Wrong &#10060";
+    ansGoodEl.innerHTML = "" + (qnIdx + 1) + ". " + "Wrong &#10060";
     showElement(ansKeyContainerEl, true);
   }
 }
 
 // penalty for a wrong answer is to reduce 10 seconds from the clock.
 function applyPenalty() {
-    timeLeft = timeLeft - penalty;
-    if (timeLeft <= 0) {
-        timeLeft = 0;
-    }
+  timeLeft = timeLeft - penalty;
+  if (timeLeft <= 0) {
+    timeLeft = 0;
+  }
 }
 
 // To show/hide the element - call showElement(el, true) - to show.
@@ -165,10 +245,13 @@ function startTimer() {
   timerInterval = setInterval(showTimeLeft, 1000);
 }
 
-// Event Listeners
+/* ----- Event Listeners --------- */
+
+// To start the quiz
 startButtonEl.addEventListener("click", startQuiz);
 
-
+// When the for submit button is clicked on the submit-score form
+formSubmitScore.addEventListener("submit", saveHighScore);
 
 /* ------------ Utility Functions  ------------- */
 
